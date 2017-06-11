ae64e62d1e4f1dc52c2dbf51886c5867eb5983c5
Shawn Stern <stern.shawn@gmail.com>
2017-06-11 00:23:31 -0700

    Wrapping Bar Chart in a Chart component with padding, and adding descriptive axis with formatted ticks

--- victory-tutorial/src/js/client.js
+++ victory-tutorial/src/js/client.js
@@ -1,19 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { VictoryBar } from 'victory';
+import {
+  VictoryAxis,
+  VictoryBar,
+  VictoryChart,
+} from 'victory';
 
 class Main extends React.Component {
   render() {
     const data = [
-      {quarter: 1, earnings: 13000},
-      {quarter: 2, earnings: 16500},
-      {quarter: 3, earnings: 14250},
-      {quarter: 4, earnings: 19000},
+      { quarter: 1, earnings: 13000 },
+      { quarter: 2, earnings: 16500 },
+      { quarter: 3, earnings: 14250 },
+      { quarter: 4, earnings: 19000 },
     ];
     return (
       <div>
         <h1>Victory Tutorial</h1>
-        <VictoryBar data={data} x="quarter" y="earnings" />
+        <VictoryChart domainPadding={20}>
+          <VictoryAxis
+            tickValues={[1, 2, 3, 4]}
+            tickFormat={["Q1", "Q2", "Q3", "Q4"]}
+          />
+          <VictoryAxis
+            dependentAxis
+            tickFormat={(x) => (`$${x / 1000}k`)}
+          />
+          <VictoryBar data={data} x="quarter" y="earnings" />
+        </VictoryChart>
       </div>
     );
   }
